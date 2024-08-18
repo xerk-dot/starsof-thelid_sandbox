aa5c19838983af73553875e31b41dd8b70e5c07e
rykr <rykr.inquiry@gmail.com>
2024-08-18 02:19:40 -0400

    actually linking the database to the UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,31 +4,21 @@ import { db } from "~/server/db";
 //every time a change occurs in the database, the page will be re-rendered
 export const dynamic = "force-dynamic";
 
-const mockUrls = [
-  "https://placehold.co/600x400/D00000/FFFFFF/png",
-  "https://placehold.co/600x400/FF00FF/000000/png",
-  "https://placehold.co/600x400/D0D0D0/FFFFFF/png",
-  "https://placehold.co/600x400/FF0F00/000000/png"
-];
 
-const mockImages = mockUrls.map((url, index)=> ({
-  id: index + 1,
-  url,
-}));
+
 export default async function HomePage() {
   
-  const posts = await db.query.posts.findMany();
-  console.log(posts);
+  const images = await db.query.images.findMany({
+    orderBy: (model, {desc}) => desc(model.id),
+  });
   
   return (
     <main className="">
       <div className="flex flex-wrap gap-2">
-        {posts.map((post) => (
-          <div key={post.id}>{post.name}</div>
-        ))}
-        {[...mockImages, ...mockImages, ...mockImages,].map((image) => (
-          <div key={image.id} className="w-48">
+        {[...images, ...images, ...images,].map((image) => (
+          <div key={image.id} className="w-48 flex flex-col">
             <img src={image.url} />
+            <div>{image.name}</div>
           </div>
         ))}
       </div>
