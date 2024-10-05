e398a9ef3cc11e47d657f7327549e85479babd52
rykr <rykr.inquiry@gmail.com>
2024-10-05 14:48:43 -0500

    return to home

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,11 +1,12 @@
 "use client";
 import { SignedIn, SignedOut, UserButton, SignInButton } from "@clerk/nextjs";
 import { SimpleUploadButton } from "./simple-upload-button";
+import Link from 'next/link';
 
 export function TopNav() {
     return (
       <nav className="flex w-full iitems-center justify-between p-4 border-b text-xl font-semibold">
-        <div>STARSOF THELID</div>
+        <Link href="/">STARSOF THELID</Link>
         <div className="flex flex-row items-center gap-4">
           <SignedOut>
             <SignInButton />
